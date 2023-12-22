5aa32e6beaf8003e5b37d2c29e1544ba98b398ef
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-12-22 22:28:45 +0800

    up

--- Side_project_1/Side_project_1/Controllers/HomeController.cs
+++ Side_project_1/Side_project_1/Controllers/HomeController.cs
@@ -3,12 +3,15 @@ using Microsoft.AspNetCore.Mvc;
 using Side_project_1.Models;
 using Side_project_1.Models.ViewModels;
 using System.Collections.Generic;
+using System.Net.Http;
 
 namespace Side_project_1.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory = null!;
+        private HomeIndexVm _HomeindexVm = new HomeIndexVm();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -17,6 +20,26 @@ namespace Side_project_1.Controllers
 
         public IActionResult Index()
         {
+            List<ThumbnailImg> thumbnailsImgs = new List<ThumbnailImg>()
+            {
+                new ThumbnailImg(){ 
+                    Location=@"C:\Users\dbdf0\source\repos\interview_project\Side_project_1\Side_project_1\wwwroot\image\index\Home\Services\coffee1.png",
+                },
+                new ThumbnailImg()
+                {
+                    Location = @"C:\Users\dbdf0\source\repos\interview_project\Side_project_1\Side_project_1\wwwroot\image\index\Home\Services\coffee2.png",
+                },
+                new ThumbnailImg()
+                {
+                    Location = @"C:\Users\dbdf0\source\repos\interview_project\Side_project_1\Side_project_1\wwwroot\image\index\Home\Services\coffee3.png",
+                },
+                new ThumbnailImg()
+                {
+                    Location = @"C:\Users\dbdf0\source\repos\interview_project\Side_project_1\Side_project_1\wwwroot\image\index\Home\Services\coffee4.png",
+                }
+            };
+
+            _HomeindexVm.thumbnailImgs = thumbnailsImgs;
             // create thumbnailImg and return to index.cshtml
             //List<HomeIndexVm> homeIndexVms = new List<HomeIndexVm> 
             //{
@@ -30,7 +53,7 @@ namespace Side_project_1.Controllers
             //        new ThunbnailImg(){ },
             //        new ThunbnailImg(){ },
             //   };
-            return View();
+            return View(_HomeindexVm);
         }
 
         public IActionResult Privacy()
--- Side_project_1/Side_project_1/Models/ViewModels/HomeIndexVm.cs
+++ Side_project_1/Side_project_1/Models/ViewModels/HomeIndexVm.cs
@@ -1,14 +1,16 @@
-﻿
+﻿using System.Collections.Generic;
+
 namespace Side_project_1.Models.ViewModels
 {
     // todo: think, fix, change structure
     public class HomeIndexVm
     {
-        public ThumbnailImg thumbnailImg { get; set; }
+        public List<ThumbnailImg> thumbnailImgs { get; set; }
     }
     public class ThumbnailImg
     {
         public string Location { get; set; }
+        public string Content { get; set; }
     }
 
 }
